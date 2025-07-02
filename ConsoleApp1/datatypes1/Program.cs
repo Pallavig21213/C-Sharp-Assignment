81d01a8ff5350da1197e2bbe9795e5360e6cc967
Pallavig21213 <pruthwigarale30@gmail.com>
2025-07-02 13:17:30 +0530

    added c sharp solution

---
+++ ConsoleApp1/datatypes1/Program.cs
@@ -0,0 +1,105 @@
+﻿
+//top level statement
+using System.Security.Cryptography;
+
+Console.WriteLine("Hello World !!!");
+
+byte v1 = 10;
+Console.WriteLine(v1);
+Console.WriteLine($"byte min value : {byte.MinValue} Max value :{byte.MaxValue} " +
+    $"size :{sizeof(byte)} bytes");
+
+short v2 = 10;
+    Console.WriteLine($"short : val : {v2}" +
+        $"min value : {short.MinValue} max value : {short.MaxValue} " +
+        $"size :  {sizeof(short)} bytes");
+
+int v3 = 4500;
+Console.WriteLine($"int val : {v3} Min Value : {int.MinValue} Max Value :{int.MaxValue} " +
+$"size : {sizeof(int)} bytes");
+
+long v4 = 9021954218;
+Console.WriteLine($"Long : Val : {v4} Min Value : {long.MinValue} Max Value : {long.MaxValue} " +
+    $"size : {sizeof(long)} bytes");
+
+float v5 = 12.5f;
+Console.WriteLine($"Float Val : {v5} Min Value : {float.MinValue} Max Value : {long .MaxValue}" +
+    $"size : { sizeof(float) } bytes");
+
+double v6 = 10.14545;
+Console.WriteLine($"double : Val : {v6} Min Value : {double.MinValue} Max Value : {double.MaxValue}" +
+  $"size : {sizeof(double)} bytes");
+
+decimal v7 = 124578.45644m;
+Console.WriteLine($"decimal : val : {v7} min val : {decimal.MinValue} max val : {decimal.MaxValue} " +
+    $"size : {sizeof(decimal)} bytes");
+
+bool status = true;
+Console.WriteLine($"bool : value : {status} size : {sizeof(bool)} bytes");
+
+char currency = '$';
+Console.WriteLine($"char : value : {currency} size : {sizeof(char)} bytes");
+
+string name = "pallavi";
+Console.WriteLine($"string : Value : {name}");
+
+// string function
+name = "Bharti";// updated value
+Console.WriteLine(name.Length); //number of characters
+
+Console.WriteLine(name.ToUpper());
+Console.WriteLine(name.ToLower());
+
+name = "     Bharti";
+Console.WriteLine(name.Trim());
+
+name = "\"vihan\"";
+Console.WriteLine(name);
+
+name = "'vihan'";
+Console.WriteLine(name);
+
+name = "\\vihaan\\";
+Console.WriteLine(name);
+
+string firstName = "vihaan";
+string lastName = "rathod";
+
+string fullName = firstName + " " + lastName;
+Console.WriteLine(fullName);
+
+fullName = firstName + "\t" + lastName;
+Console.WriteLine(fullName);
+
+string middleName = "vikhul";
+
+fullName = string.Concat(firstName, " ", middleName, " ", lastName);
+Console.WriteLine(fullName);
+
+
+fullName = string.Join("-", firstName, middleName, lastName);
+Console.WriteLine(fullName);
+
+//placeholder syntax
+fullName = string.Format("Full Name : {0} {1} {2}", firstName, middleName, lastName);
+Console.WriteLine(fullName);
+
+fullName = $"Full Name : {firstName} {middleName} {lastName}";
+Console.WriteLine(fullName);
+
+string email = "vihaan@gmail.com";
+int index = email.IndexOf('@');
+Console.WriteLine($"Index of @ : {index}");
+
+
+   string username = email.Substring(0, index);
+Console.WriteLine($"username : {username}");
+
+
+
+
+
+Console.Read();
+        
+
+
---
+++ ConsoleApp1/operators/Program.cs
@@ -0,0 +1,138 @@
+﻿using System.Net.Mail;
+
+int a = 10;
+Console.WriteLine($"{a}");
+
+int b = 2;
+
+int add = a + b;
+Console.WriteLine($"Addiation : {add} ");
+
+int sub = a - b;
+Console.WriteLine($"Substration : {sub} ");
+
+int mul = a * b;
+Console.WriteLine($"Multiplication : {mul} ");
+
+int div = a / b;
+Console.WriteLine($"division : {div} ");
+
+int mod = a % b;
+Console.WriteLine($"Remainder : {mod}");
+
+b = 3;
+mod = a % b;
+Console.WriteLine($"Reminder : {mod}");
+
+Console.WriteLine($"a : {a} ");
+a++; // a = a + 1;
+Console.WriteLine($"a++ : {a} ");
+
+// Console.WriteLine($"a : {a++}"); // a++ // post increment // 11
+
+Console.WriteLine($"a : {++a}"); // ++a // pre increment // 12
+Console.WriteLine($"a : {a}"); // 12
+
+a--; // a = a - 1;
+Console.WriteLine($"a : {a}");
+
+
+// Console.WriteLine($"a : {--a}"); // pre decrement  // 10
+Console.WriteLine($"a : {a--}"); // post decrement  // 11
+Console.WriteLine($"a : {a}"); // 10
+
+// a = 10
+//15
+
+a -= 10;
+Console.WriteLine($"a : {a}");
+
+a *= 3;
+Console.WriteLine($"a : {a}"); // 15
+
+a = a / 3;
+
+a = 10;
+
+bool b1 = a == 10;
+Console.WriteLine($"a == 10 : {b1}");
+
+b1 = a != 10;
+Console.WriteLine($"a != 10 : {b1}");
+
+// and operator &&
+
+b1 = (a == 10) && (b == 10);
+Console.WriteLine($"({a} == 10) && ({b} == 10) : {b1}"); 
+
+
+b1 = (a == 10) && (b != 10);
+Console.WriteLine($"({a} == 10) && ({b} != 10) : {b1}");
+
+// or operator ||
+b1 = (a == 10) || (b == 10);
+Console.WriteLine($"({a} == 10) || ({b} == 10) : {b1}"); 
+
+b1 = (b == 10) & (a == 10);
+Console.WriteLine($"({b} == 10) & ({a} == 10) : {b1}"); 
+
+b1 = (a == 10) | (b == 10);
+Console.WriteLine($"({a} == 10) | ({b} == 10) : {b1}");
+
+
+// verbatim literal = removes meaning of back slashes
+// generally we use with file folder path
+
+short v1 = 10;
+Console.WriteLine($"short v1 : {v1}");
+
+
+int v2 = v1; // short => int 
+Console.WriteLine($"int v2 : {v2}");
+
+float v3 = v2; // int => float
+Console.WriteLine($"float v3 : {v3}");
+
+// using type cast operator
+v3 = 10.5f;
+int v4 = (int)v3; // float => int
+Console.WriteLine($"int v4 : {v4}");
+
+string v5 = "1000";
+// int v6 = v5; // string => int
+int v6 = Convert.ToInt32(v5); // converting string to int
+
+Console.WriteLine($"int v6 : {v6}");
+
+v5 = "true";
+bool v7 = Convert.ToBoolean(v5); // string => bool
+Console.WriteLine($"bool v7 : {v7}");
+
+// using Parse(), TryParse() methods
+v5 = "120";
+
+int v8 = int.Parse(v5); // string => int
+Console.WriteLine($"int v8 : {v8}");
+
+// need of TypeParse() method
+
+string s = "fdasfdsf";
+
+// int i = Convert.ToInt32(s);
+// int i = int.Parse(s);
+
+int i; 
+bool b = int.TryParse(s, out i);
+
+Console.WriteLine($"Is conversion success : {b} int i : {i}");
+
+s = "500";
+b = int.TryParse(s, out i);
+Console.WriteLine($"Is conversion success : {b} int i : {i}");
+
+
+
+
+
+Console.Read();
+
